50ccdd11d58fb919bae0d4b3515417100d749e9c
stian <stian@192.168.0.191>
2022-09-08 14:33:50 +0200

    Endring i Oppgaver

--- Oblig1/src/no/hvl/dat100/O2.java
+++ Oblig1/src/no/hvl/dat100/O2.java
@@ -7,68 +7,56 @@ public class O2 {
 
 	public static void main(String[] args) {
 		
-		String inntektTxt = showInputDialog("Hva er inntekten din?");
-		
-		int inntekt = parseInt(inntektTxt);
-		
-		double trinnskatt;
-		double sats;
-		String utTxt;
-		
-		if(0 < inntekt && inntekt <= 190349) {
+		for (int i = 1; i <= 10; i ++) {
 			
-			utTxt = "Siden inntekten din er " + inntekt + "kr og er under 190349kr vil "
-					+ "du ikke ha trinnskatt";
+			int poeng;
 			
-		}  else if (inntekt <= 267899) {
-			
-			sats = 1.7 / 100;
-			trinnskatt = inntekt * sats;
-			utTxt = "Siden inntekten din er " + inntekt + "kr og er mellom 190350kr og 267899kr er "
-					+ "satsen din 1.7% og da blir trinnskatten din "
-					+ trinnskatt + "kr";
-			
-		} else if (inntekt <= 643799) {
-			
-			sats = 4.0 / 100;
-			trinnskatt = inntekt * sats;
-			utTxt = "Siden inntekten din er " + inntekt + "kr og er mellom 267900kr og 643799kr er "
-					+ "satsen din 4.0% og da blir trinnskatten din "
-					+ trinnskatt + "kr";
-			
-		} else if (inntekt <= 969199) {
-			
-			sats = 13.4 / 100;
-			trinnskatt = inntekt * sats;
-			utTxt = "Siden inntekten din er " + inntekt + "kr og er mellom 643800kr og 969199kr er "
-					+ "satsen din 13.4% og da blir trinnskatten din "
-					+ trinnskatt + "kr";
-			
-			
-		} else if (inntekt <= 1999999) {
-			
-			sats = 16.4 / 100;
-			trinnskatt = inntekt * sats;
-			utTxt = "Siden inntekten din er " + inntekt + "kr og er mellom 969200kr og 1999999kr er "
-					+ "satsen din 16.4% og da blir trinnskatten din "
-					+ trinnskatt + "kr";
-			
-			
-		} else if (2000000 <= inntekt) {
-			
-			sats = 17.4 / 100;
-			trinnskatt = inntekt * sats;
-			utTxt = "Siden inntekten din er " + inntekt + "kr og er over 200000kr er "
-					+ "satsen din 17.4% og da blir trinnskatten din "
-					+ trinnskatt + "kr";
-			
-		} else {
-			
-			utTxt = "Du har skrevet en ugyldig inntekt";
-			
-		}
-		
-		showMessageDialog(null, utTxt);
-		
+			do {
+				String poengTxt = showInputDialog("Skriv inn Poengsum");
+				
+				poeng = parseInt(poengTxt);
+				
+				char karakter;	
+					if(0 <= poeng && poeng <= 39) {
+					
+						karakter = 'F';
+					
+						showMessageDialog(null, "Du får karakteren " + karakter);
+					
+					} else if(poeng <= 49) {
+					
+						karakter = 'E';
+					
+						showMessageDialog(null, "Du får karakteren " + karakter);
+					
+					} else if(poeng <= 59) {
+					
+						karakter = 'D';
+					
+						showMessageDialog(null, "Du får karakteren " + karakter);
+					
+					} else if(poeng <= 79) {
+					
+						karakter = 'C';
+					
+						showMessageDialog(null, "Du får karakteren " + karakter);
+					
+					} else if(poeng <= 89) {
+					
+						karakter = 'B';
+					
+						showMessageDialog(null, "Du får karakteren " + karakter);
+					
+					} else if(poeng <= 100) {
+					
+						karakter = 'A';
+					
+						showMessageDialog(null, "Du får karakteren " + karakter);
+					
+					} 
+			} while (0 > poeng || poeng > 100);
+		}		
 	}
 }
+
+
